de4df69c6bba28301d403bfcf6597c286bb1575e
Bencso <158219939+bencso@users.noreply.github.com>
2024-11-07 17:07:50 +0100

    UI update, refactoring

--- js/main.js
+++ js/main.js
@@ -1,113 +1,154 @@
 const directions = ["up", "down", "right", "left"];
 let running = false;
-const dpPhElement = document.querySelector(".dp-ph");
-const keyboardBtns = document.querySelectorAll("[data-key]");
-const timerElement = document.querySelector("#timer");
-const actionBtn = document.querySelector("#actionBtn");
 let timerInterval;
 let startTime = 0;
 let reactionTimes = [];
 let points = 0;
+let direction = "";
+let preloadedDirections = preloadDirections();
+
+const dpPhElement = document.querySelector(".game__placeholder");
+const keyboardBtns = document.querySelectorAll("[data-key]");
+const timerElement = document.querySelector("#timer");
+const actionBtn = document.querySelector("#actionBtn");
 
 function startGame() {
-    running = true;
-    getDirection();
-    startTimer();
-    actionBtn.textContent = "Stop";
-    actionBtn.removeEventListener("click", startGame);
-    actionBtn.addEventListener("click", stopGame);
-    document.querySelector(".timer").classList.remove("blink");
+  running = true;
+  updateActionButton("Stop", stopGame);
+  toggleTimerBlink(false);
+  getDirection();
 }
 
 function stopGame() {
-    running = false;
-    clearInterval(timerInterval);
-    calculateAvgRT();
-    actionBtn.textContent = "Start";
-    actionBtn.removeEventListener("click", stopGame);
-    actionBtn.addEventListener("click", startGame);
-    keyboardBtns.forEach((btn) => {
-        btn.removeEventListener("click", handleClick);
-    });
-    window.removeEventListener("keydown", handleKeyDown);
-    document.querySelector(".timer").classList.add("blink");
-    points = 0;
+  running = false;
+  clearInterval(timerInterval);
+  calculateAvgRT();
+  updateActionButton("Start", startGame);
+  removeEventListeners();
+  toggleTimerBlink(true);
+  points = 0;
+}
+
+function preloadDirections() {
+  return Array.from({ length: 2 }, randomDirection);
+}
+
+function updateActionButton(text, handler) {
+  actionBtn.textContent = text;
+  actionBtn.onclick = handler;
 }
 
 function randomDirection() {
-    const randomIndex = Math.floor(Math.random() * directions.length);
-    return directions[randomIndex];
+  return directions[Math.floor(Math.random() * directions.length)];
 }
 
 function getDirection() {
-    if (!running) return;
-    startTime = new Date().getTime();
-    const direction = randomDirection();
-    dpPhElement.textContent = direction;
-    keyboardBtns.forEach((btn) => {
-        btn.removeEventListener("click", handleClick);
-        btn.addEventListener("click", handleClick);
-    });
-    window.removeEventListener("keydown", handleKeyDown);
-    window.addEventListener("keydown", handleKeyDown);
+  if (!running) return;
+  startTime = Date.now();
+  direction = preloadedDirections.shift();
+  preloadedDirections.push(randomDirection());
+  updateSlider(direction);
+  resetTimer();
+  addEventListeners();
 }
 
 function handleClick() {
-    const keyDirection = this.dataset.key;
-    checkDirection(keyDirection, dpPhElement.textContent);
+  checkDirection(this.dataset.key);
+}
+
+function updateSlider(direction) {
+  dpPhElement.innerHTML = `
+        <span class="fa6-solid--${direction}-long"></span>
+        <span class="fa6-solid--${preloadedDirections[0]}-long"></span>
+    `;
 }
 
 function handleKeyDown(event) {
-    let keyDirection = event.key.toLowerCase();
-    if (keyDirection.includes("arrow")) {
-        keyDirection = keyDirection.replace("arrow", "").toLowerCase();
-    }
-    if (keyDirection === "escape") {
-        stopGame();
-    }
-    if (keyDirection === "enter") {
-        startGame();
-    }
-    if (directions.includes(keyDirection)) {
-        checkDirection(keyDirection, dpPhElement.textContent);
-    }
+  const keyDirection = event.key.toLowerCase().replace("arrow", "");
+  if (keyDirection === "escape") stopGame();
+  if (directions.includes(keyDirection)) checkDirection(keyDirection);
 }
 
-function checkDirection(keyDirection, direction) {
-    console.table({ key: keyDirection, random: direction, isEqual: keyDirection === direction });
-    document.querySelector(`[data-key="${keyDirection}"]`).classList.add("hovered");
-    if (keyDirection === direction) {
-        dpPhElement.style.color = "green";
-        points++;
-    } else {
-        dpPhElement.style.color = "red";
-    }
-    setTimeout(() => {
-        dpPhElement.style.color = "black";
-        document.querySelector(`[data-key="${keyDirection}"]`).classList.remove("hovered");
-        getDirection();
-    }, 100);
-    const reactionTime = new Date().getTime() - startTime;
-    reactionTimes.push(reactionTime);
+function checkDirection(keyDirection) {
+  const isCorrect = keyDirection === direction;
+  dpPhElement.style.color = isCorrect ? "#00FF00" : "#FF0000";
+  if (isCorrect) points++;
+  highlightButton(keyDirection);
+  setTimeout(() => {
+    dpPhElement.style.color = "#FAF0E6";
+    unhighlightButton(keyDirection);
+    getDirection();
+  }, 100);
+  reactionTimes.push(Date.now() - startTime);
+}
+
+function highlightButton(keyDirection) {
+  document
+    .querySelector(`[data-key="${keyDirection}"]`)
+    .classList.add("hovered");
+}
+
+function unhighlightButton(keyDirection) {
+  document
+    .querySelector(`[data-key="${keyDirection}"]`)
+    .classList.remove("hovered");
 }
 
 function startTimer() {
-    let time = 20;
-    timerElement.textContent = time.toFixed(2).padStart(5, '0');
-    timerInterval = setInterval(() => {
-        time -= 0.01;
-        timerElement.textContent = time.toFixed(2).padStart(5, '0');
-        if (time <= 0) {
-            stopGame();
-        }
-    }, 10);
+  let time = 20;
+  timerElement.textContent = formatTime(time);
+  timerInterval = setInterval(() => {
+    time -= 0.01;
+    timerElement.textContent = formatTime(time);
+    if (time <= 0) stopGame();
+  }, 10);
 }
 
+function resetTimer() {
+  clearInterval(timerInterval);
+  startTimer();
+}
+
+function formatTime(time) {
+  return time.toFixed(2).padStart(5, "0");
+}
 
 function calculateAvgRT() {
-    const avgRT = reactionTimes.reduce((acc, curr) => acc + curr, 0) / reactionTimes.length;
-    const minRT = Math.min(...reactionTimes);
-    const successRate = (points / reactionTimes.length) * 100;
-    dpPhElement.textContent = `AVG reaction time: ${avgRT.toFixed(2)}ms, Fastest reaction: ${minRT}ms, Score: ${reactionTimes.length}/${points}, Success rate: ${successRate.toFixed(2)}%`;
-    reactionTimes = [];
+  const avgRT =
+    reactionTimes.reduce((acc, curr) => acc + curr, 0) / reactionTimes.length;
+  const minRT = Math.min(...reactionTimes);
+  const successRate = (points / reactionTimes.length) * 100;
+  dpPhElement.textContent =
+    reactionTimes.length > 0
+      ? `AVG reaction time: ${avgRT.toFixed(
+          2
+        )}ms, Fastest reaction: ${minRT} ms, Score: ${
+          reactionTimes.length
+        }/${points}, Success rate: ${successRate.toFixed(2)}%`
+      : `AVG reaction time: 0ms, Fastest reaction: 0 ms, Score: 0/0, Success rate: 0%`;
+  reactionTimes = [];
 }
+
+function addEventListeners() {
+  keyboardBtns.forEach((btn) => {
+    btn.removeEventListener("click", handleClick);
+    btn.addEventListener("click", handleClick);
+  });
+  window.removeEventListener("keydown", handleKeyDown);
+  window.addEventListener("keydown", handleKeyDown);
+}
+
+function removeEventListeners() {
+  keyboardBtns.forEach((btn) => btn.removeEventListener("click", handleClick));
+  window.removeEventListener("keydown", handleKeyDown);
+}
+
+function toggleTimerBlink(shouldBlink) {
+  document
+    .querySelector(".game__timer")
+    .classList.toggle("game__timer--blink", shouldBlink);
+}
+
+window.addEventListener("keydown", (event) => {
+  if (event.key.toLowerCase() === "enter" && !running) startGame();
+});
